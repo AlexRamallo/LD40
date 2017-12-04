2ab5853829c72be944ef4b713c089f06f0a6d4ab
Alejandro Ramallo <alejandroramallo@gmail.com>
2017-12-03 19:28:45 -0500

    Tweaked more gameplay stuff

--- export/windows/neko/haxe/ApplicationMain.hx
+++ export/windows/neko/haxe/ApplicationMain.hx
@@ -515,7 +515,7 @@ class ApplicationMain {
 		
 		config = {
 			
-			build: "302",
+			build: "304",
 			company: "HaxeFlixel",
 			file: "LD40",
 			fps: 60,
--- source/PlayData.hx
+++ source/PlayData.hx
@@ -38,7 +38,7 @@ class PlayData
 	
 	public var memory:Int = 5; //memory of your viewers
 	
-	public var money:Int = 50000;
+	public var money:Int = 0;
 	
 	public var stunt_history:Array<Stunt> = [];
 	
@@ -135,7 +135,7 @@ class PlayData
 		}
 		
 		//Followers gained from the stunt
-		var followers_from_stunt = (250000 * (danger / 100)) + (250000 * risk);
+		var followers_from_stunt = (50000 * (danger / 100)) + (50000 * risk);
 		
 		/*var loss_from_boredom = 0;
 		if(net_risk <= 0 && net_danger <= 0){
@@ -166,7 +166,8 @@ class PlayData
 			  (followers_from_stunt / loss_from_repeat) + stupidity_bonus
 			- (loss_from_recent * followers_from_stunt)
 		);
-		
+		if (damage > 99)
+			damage = 99;
 		stunt.result_damage = damage;
 		stunt_history.push(stunt);
 		giveRewards(stunt);
@@ -286,7 +287,6 @@ class PlayData
 		if(health <= 0){
 			view.died();
 			prog.died();
-			prog.alert("You're dead :'(", "Guess what?");
 		}
 	}
 	
@@ -298,37 +298,96 @@ class PlayData
 				new Stunt_Act("eat",									0,		2,		0,			0, [
 					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
 					new Stunt_Subject("a burger",						1,		2,		0,			0),
-					new Stunt_Subject("some yogurt",					5,		2,		0,			0),
-					new Stunt_Subject("a condiment sundae",				20,		5,		2,			15),
-					new Stunt_Subject("some cinnamon",					25,		2,		3,			30),
-					new Stunt_Subject("a habanero pepper",				35,		8,		5,			100),
-					new Stunt_Subject("a ghost pepper",					50,		15,		7,			250),
-					new Stunt_Subject("a carolina reaper pepper",		55,		15,		9,			500)
+					new Stunt_Subject("a habanero pepper",				5,		2,		2,			0),
+					new Stunt_Subject("a ghost pepper",					5,		2,		4,			15),
+					new Stunt_Subject("dog poop",						8,		5,		8,			100),
+					new Stunt_Subject("a paperclip",					30,		15,		9,			30),
+					new Stunt_Subject("an innocent frog",				30,		70,		9,			250),
+					new Stunt_Subject("your neighbor",					85,		85,		9,			500)
 				]),
 				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
-				new Stunt_Act("surprise hug",							5,		5,		0,			0, [
+				new Stunt_Act("surprise hug",							15,		10,		4,			0, [
 					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
-					new Stunt_Subject("your friend",					0,		0,		0,			0),
-					new Stunt_Subject("your mom",						-100,	-100,	0,			0),
-					new Stunt_Subject("your coworker",					10,		2,		3,			50),
-					new Stunt_Subject("your boss",						40,		20,		5,			75),
-					new Stunt_Subject("a stranger",						50,		25,		6,			250),
-					new Stunt_Subject("a hobo",							70,		20,		7,			500),
-					new Stunt_Subject("a stray dog",					20,		40,		8,			1000),
-					new Stunt_Subject("a stray cat",					60,		40,		8,			1200),
-					new Stunt_Subject("a trash panda",					50,		60,		8,			2000),
-					new Stunt_Subject("a bear",							80,		80,		9,			5000),
-					new Stunt_Subject("a cocaine bear",					99,		99,		9,			10000),
+					new Stunt_Subject("a stranger",						30,		15,		4,			0),
+					new Stunt_Subject("a stray dog",					25,		40,		5,			0),
+					new Stunt_Subject("a stray cat",					65,		40,		7,			0),
+					new Stunt_Subject("a hobo",							35,		25,		6,			50),
+					new Stunt_Subject("a trash panda",					50,		50,		6,			250),
+					new Stunt_Subject("a bear",							70,		20,		7,			500),
+					new Stunt_Subject("a cocaine bear",					90,		95,		9,			1000)
 				]),
 				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
-				new Stunt_Act("race",									10,		10,		0,			0, [
+				new Stunt_Act("play",									10,		5,		0,			0, [
 					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
-					new Stunt_Subject("on foot",						5,		5,		0,			0),
-					new Stunt_Subject("on a bike",						15,		8,		2,			100),
-					new Stunt_Subject("on a moped",						20,		10,		3,			250),
-					new Stunt_Subject("go karts",						10,		20,		3,			500),
-					new Stunt_Subject("in a car",						35,		70,		8,			1000),
-					new Stunt_Subject("airplanes",						75,		99,		9,			5000),
+					new Stunt_Subject("video games",					1,		1,		0,			0),
+					new Stunt_Subject("hide-n-seek",					10,		20,		2,			100),
+					new Stunt_Subject("russian roulette",				16.6,	10,		3,			250),
+					new Stunt_Subject("mumblety peg",					33.3,	20,		3,			500),
+					new Stunt_Subject("in traffic",						60,		80,		8,			100),
+				]),
+				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
+				new Stunt_Act("fall",									20,		30,		6,			0, [
+					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
+					new Stunt_Subject("on a banana peel",				15,		8,		2,			100),
+					new Stunt_Subject("from a moving car",				60,		5,		0,			0),
+					new Stunt_Subject("for a scam",						50,		30,		3,			250),
+					new Stunt_Subject("in love",						70,		60,		9,			500),
+				]),
+				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
+				new Stunt_Act("chase",									20,		10,		9,			0, [
+					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
+					new Stunt_Subject("a stranger down the street",		15,		30,		7,			100),
+					new Stunt_Subject("waterfalls",						60,		5,		7,			0),
+					new Stunt_Subject("a chocolate high",				10,		20,		3,			500),
+					new Stunt_Subject("a slow tiger",					10,		60,		3,			250),
+					new Stunt_Subject("a greasy tiger",					70,		80,		9,			250),
+					new Stunt_Subject("your dreams",					0,		0,		0,			500),
+				]),
+				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
+				new Stunt_Act("fight",									30,		20,		9,			0, [
+					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
+					new Stunt_Subject("a mannequin",					5,		5,		9,			0),
+					new Stunt_Subject("small animals",					6,		6,		9,			500),
+					new Stunt_Subject("a birthday clown",				15,		15,		9,			100),
+					new Stunt_Subject("a funeral clown",				40,		35,		9,			250),
+					new Stunt_Subject("an alligator",					50,		65,		9,			500),
+					new Stunt_Subject("a martial artist",				80,		80,		9,			500),
+					new Stunt_Subject("two martial artists",			160,	160,	18,			500),
+				]),
+				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
+				new Stunt_Act("prank",									5,		5,		3,			0, [
+					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
+					new Stunt_Subject("your parents",					2,		5,		3,			500),
+					new Stunt_Subject("a nigerian prince",				2,		2,		1,			100),
+					new Stunt_Subject("your roommate",					5,		15,		2,			0),
+					new Stunt_Subject("your neighbor",					15,		8,		2,			100),
+					new Stunt_Subject("the police",						90,		94,		9,			250),
+				]),
+				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
+				new Stunt_Act("run",									10,		10,		0,			0, [
+					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
+					new Stunt_Subject("downhill",						15,		30,		5,			0),
+					new Stunt_Subject("for governor",					45,		45,		3,			500),
+					new Stunt_Subject("from the law",					65,		75,		2,			100),
+					new Stunt_Subject("out of toilet paper",			99,		99,		11,			100),
+				]),
+				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
+				new Stunt_Act("crush",									10,		1,		3,			0, [
+					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
+					new Stunt_Subject("your own hopes and dreams",		1,		1,		9,			100),
+					new Stunt_Subject("a soda can",						5,		5,		1,			0),
+					new Stunt_Subject("a beer can",						10,		5,		1,			0),
+					new Stunt_Subject("a watermelon",					10,		20,		3,			500),
+					new Stunt_Subject("a stick of dynamite",			85,		97,		9,			100),
+				]),
+				//			  NAME,										RISK,	DANGER,	STUPIDITY,	PRICE
+				new Stunt_Act("build",									3,		5,		0,			0, [
+					//				  NAME								%RISK	DANGER	STUPIDITY	PRICE
+					new Stunt_Subject("a minecraft house",				5,		5,		2,			0),
+					new Stunt_Subject("a dam in the sewers",			10,		20,		3,			500),
+					new Stunt_Subject("castles in the sky",				25,		20,		2,			100),
+					new Stunt_Subject("a secret clubhouse",				30,		25,		4,			500),
+					new Stunt_Subject("the linux kernel",				75,		82,		9,			500),
 				]),
 				////////////////////
 			];
